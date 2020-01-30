acccd3a541ab63c6209f89940f1831cba1d2f5f8
mbosselut <duchmol7@hotmail.com>
2020-01-30 20:00:31 +0100

    Create MealsList component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import DateForm from './DateForm';
+import MealsList from './MealsList';
 
 const App = (): JSX.Element => {
   return (
     <div>
       <DateForm />
+      <MealsList
+        meals={[
+          { date: new Date(), meal: 'Pasta', diet: 'Vegetarian' },
+          { date: new Date(), meal: 'Ribs', diet: 'Meat' }
+        ]}
+      />
     </div>
   );
 };
