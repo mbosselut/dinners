b2bdf15cc757035d1f9a14ffcefc927fd17a0728
mbosselut <duchmol7@hotmail.com>
2020-01-30 17:09:19 +0100

    Create DateForm component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,36 +1,10 @@
-import React, { useState, FormEvent } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import React from 'react';
+import DateForm from './DateForm';
 
 const App = (): JSX.Element => {
-  const [startDate, setStartDate] = useState<any>(new Date());
-
-  const handleChange = (date: Date) => {
-    setStartDate(date);
-  };
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    let main = startDate;
-    console.log(main);
-  };
   return (
     <div>
-      {/* <DatePicker selected={startDate} onChange={date => setStartDate(date)} /> */}
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Select Date: </label>
-          <DatePicker
-            selected={startDate}
-            onChange={handleChange}
-            name="startDate"
-            dateFormat="dd/mm/yyyy"
-          />
-        </div>
-        <div className="form-group">
-          <button className="btn btn-success">Add Date</button>
-        </div>
-      </form>
+      <DateForm />
     </div>
   );
 };
