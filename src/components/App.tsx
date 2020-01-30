c58c4c7f35abf42c9577ff545b46af714fddcf68
mbosselut <duchmol7@hotmail.com>
2020-01-30 16:09:50 +0100

    Implement DatePicker

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, FormEvent } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-class App extends Component {
-  render() {
-    return <div>Hi there</div>;
-  }
-}
+const App = (): JSX.Element => {
+  const [startDate, setStartDate] = useState<any>(new Date());
+
+  const handleChange = (date: Date) => {
+    setStartDate(date);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    let main = startDate;
+    console.log(main);
+  };
+  return (
+    <div>
+      {/* <DatePicker selected={startDate} onChange={date => setStartDate(date)} /> */}
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Select Date: </label>
+          <DatePicker
+            selected={startDate}
+            onChange={handleChange}
+            name="startDate"
+            dateFormat="dd/mm/yyyy"
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-success">Add Date</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default App;
